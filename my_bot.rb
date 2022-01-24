e6d3824f5bb7fee142e78c4fd9590f53de6babef
Morgan Schweers <cyberfox@gmail.com>
2022-01-24 01:02:36 -0800

    Include an example of breaking out handlers.

--- my_bot.rb
+++ my_bot.rb
@@ -1,28 +1,48 @@
 require_relative 'basic_bot'
+require 'active_support/concern'
+
+module AddEchoe
+  extend ActiveSupport::Concern
+  included do
+    command '/echoe' do |_, text, channel, info, client|
+      client.chat_postMessage(channel: channel, text: "Did a reverse!")
+      { response_type: 'ephemeral', text: text.reverse }
+    end
+  end
+end
+
+module TimeHandler
+  extend ActiveSupport::Concern
+  included do
+    message /(?i)what time (is it|it is)/ do |text, match, event, channel, client|
+      client.chat_postMessage(channel: channel, text: "The time is now #{Time.now.strftime("%B %-d, %Y at at %I:%M%p")}")
+    end
+  end
+end
+
+module GreetingsHandler
+  extend ActiveSupport::Concern
+  included do
+    mention /(?i)hello/ do |text, match, event, channel, client|
+      client.chat_postMessage(channel: channel, text: "Hello <@#{event.user}>!")
+      true
+    end
+
+    mention /(?i)good\s?bye/ do |text, match, event, channel, client|
+      client.chat_postMessage(channel: channel, text: "Catch you soon, <@#{event.user}>!")
+      true
+    end
+  end
+end
 
 class MyBot < BasicBot
   command '/echo' do |_, text, channel, info, client|
     { response_type: 'ephemeral', text: text }
   end
 
-  command'/echoe' do |_, text, channel, info, client|
-    client.chat_postMessage(channel: channel, text: "Did a reverse!")
-    { response_type: 'ephemeral', text: text.reverse }
-  end
-
-  mention /(?i)hello/ do |text, match, event, channel, client|
-    client.chat_postMessage(channel: channel, text: "Hello <@#{event.user}>!")
-    true
-  end
-
-  mention /(?i)good\s?bye/ do |text, match, event, channel, client|
-    client.chat_postMessage(channel: channel, text: "Catch you soon, <@#{event.user}>!")
-    true
-  end
-
-  message /(?i)what time (is it|it is)/ do |text, match, event, channel, client|
-    client.chat_postMessage(channel: channel, text: "The time is now #{Time.now.strftime("%B %-d, %Y at at %I:%M%p")}")
-  end
+  include AddEchoe
+  include TimeHandler
+  include GreetingsHandler
 end
 
 MyBot.start
